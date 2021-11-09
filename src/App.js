4a70327b02aed08db13b2a649602618ed096e6e8
fiil <filipisee@hotmail.com>
2021-11-09 16:36:37 -0300

    added some component files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 // Components
+import Modal from "./components/Modal/Modal";
+import DataInput from "./components/DataInput/DataInput";
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <Modal />
+      <DataInput />
     </>
   );
 }
