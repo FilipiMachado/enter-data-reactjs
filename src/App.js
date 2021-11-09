23742758658a2022c3eb0a471255e0d1768761f8
fiil <filipisee@hotmail.com>
2021-11-09 16:57:33 -0300

    some changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@
 import Modal from "./components/Modal/Modal";
 import DataInput from "./components/DataInput/DataInput";
 // Styles
-import "App.css";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
-      <Modal />
       <DataInput />
+      <Modal />
     </>
   );
 }
