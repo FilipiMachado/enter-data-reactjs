ef69518eff73657b77fb84bc9bf261da87b1e507
Filipi <filipisee@hotmail.com>
2021-11-17 16:22:56 -0300

    added Data component

---
+++ src/components/Data/Data.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Data = () => {
+  const data = [
+    { user: "MahBoeh", age: "20", id: 1 },
+    { user: "Chrono", age: "48", id: 2 },
+    { user: "Biluguinha", age: "35", id: 3 },
+  ];
+
+  return (
+    <div>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Data;
--- src/components/DataInput/DataInput.jsx
+++ src/components/DataInput/DataInput.jsx
@@ -1,33 +1,40 @@
 import React, { useState } from "react";
+// Components
+import Data from "../Data/Data";
 // Styles
 import "./DataInput.css";
 
 const DataInput = () => {
-  const [dataInput, setDataInput] = useState();
+  const [userDataInput, setUserDataInput] = useState();
 
-  const dataInputHandler = (data) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setUserDataInput(() => {});
+  };
 
   return (
     <>
       <div className="data-container">
-        <form onSubmit={() => {}} className="data-container__block">
-          <p className="data-container__user">Username</p>
+        <form onSubmit={handleSubmit} className="data-container__block">
+          <p className="data-container__user">Username:</p>
           <input
             className="data-container__input"
             type="text"
             placeholder="Enter your name..."
-          ></input>
-          <p className="data-container__user">Age (Years)</p>
+          />
+          <p className="data-container__user">Age (Years):</p>
           <input
             className="data-container__input"
             type="number"
-            placeholder="Add you age..."
-          ></input>
+            placeholder="Enter your age..."
+          />
           <button type="submit" className="data-container__button">
             Add User
           </button>
         </form>
       </div>
+      <Data />
     </>
   );
 };
